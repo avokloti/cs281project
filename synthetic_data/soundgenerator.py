17ac659130c6af9eee48eaebf0271cf72cc3ed9a
Tyler Piazza <tyler_piazza@college.harvard.edu>
2019-12-07 14:09:48 -0500

    basic version of spectrograms works for synthetic data (uses Irina's code)

--- synthetic_data/soundgenerator.py
+++ synthetic_data/soundgenerator.py
@@ -1,5 +1,6 @@
 # Tyler Piazza
 # Python 3
+# November-December 2019
 # goal is to artificially generate sound waves that sound like birds, using Python
 
 """
@@ -15,6 +16,7 @@ import random
 import numpy as np
 from scipy.io import wavfile
 from scipy.signal import chirp, spectrogram
+from matplotlib import pyplot as plt
 
 #SAMPLERATE = 100000 # this was originall set to 44100; maybe tweaking this will get better results with higher frequency? It's a performance tradeoff
 SAMPLERATE = 44100 # this is the standard
@@ -130,7 +132,7 @@ def produce_samples_v2(freqrange=[C4FREQ, C4FREQ],
   # length and amplitude are hopefully self explanitory (time in seconds, and something proportional to amplitude)
   # noiselevel is a number in [0.,1.], it represents how much to bias the noise when it comes to adding the sounds
   # numsamples is how many samples, file_prefix is the prefix for the files that you save these with
-
+  returnarray = []
   t = np.linspace(0, int(length), int(SAMPLERATE * length))
   for i in range(numsamples):
     # use my create_advanced_chirp_array, which is already normalized
@@ -142,6 +144,70 @@ def produce_samples_v2(freqrange=[C4FREQ, C4FREQ],
     wavfile.write(fileprefix + "noise_alone_" + str(i) + ".wav", SAMPLERATE, y_random)
     # both combined
     wavfile.write(fileprefix + "bird_noise_mix_" + str(i) + ".wav", SAMPLERATE, y_chirp + y_random)
+
+    returnarray.append([y_chirp, y_random, y_chirp + y_random])
+  # also return an array of the triplets of values
+  return returnarray
+
+def makeSpectrogram(s, framerate=SAMPLERATE):
+    """
+    note: this code was originally from Irina's construct_dataset.py
+    framerate should be samplingrate, which I have as SAMPLERATE
+    s should be
+    """
+
+    # create hanning window of size n
+    N = len(s)
+    n = 400
+    g = np.hanning(n)
+    freqs = np.fft.fftfreq(n, 1/framerate)
+    t = np.arange(N) * 1/framerate
+
+    # move by more than one per iteration
+    inds = np.arange(0, N - n, n/4)
+
+    # save amplitudes at specific times
+    spec_abs = np.zeros((len(inds), np.int(n/2)))
+    spec_real = np.zeros((len(inds), np.int(n/2)))
+    spec_imag = np.zeros((len(inds), np.int(n/2)))
+
+    for i in np.arange(len(inds)):
+        #print(str(inds[i]) + "  " + str(inds[i]+n))
+        fw = np.fft.fft(s[int(inds[i]):int(inds[i]+n)] * g)
+        fw_abs = np.abs(fw)
+        spec_abs[i, :] = fw_abs[0:np.int(n/2)]
+        spec_imag[i, :] = fw.imag[0:np.int(n/2)]
+        spec_real[i, :] = fw.real[0:np.int(n/2)]
+
+    return [t, freqs, spec_abs]
+
+def showspectrogram(spec_abs, label="Sound"):
+  # show spectrogram
+  plt.figure()
+
+  plt.subplot(3, 1, 1)
+  plt.imshow(np.transpose(spec_abs[:, 0:100]), aspect='auto')
+  plt.gca().invert_yaxis()
+  plt.xlabel('Time')
+  plt.ylabel('Frequency')
+  plt.title('Spectrogram of ' + label)
+  """
+  plt.subplot(3, 1, 2)
+  plt.imshow(np.transpose(specs_bg[:, 0:100]), aspect='auto')
+  plt.gca().invert_yaxis()
+  plt.xlabel('Time')
+  plt.ylabel('Frequency')
+  plt.title('Spectrogram of Background (Noise)')
+
+  plt.subplot(3, 1, 3)
+  plt.imshow(np.transpose(specs_mix[:, 0:100]), aspect='auto')
+  plt.gca().invert_yaxis()
+  plt.xlabel('Time')
+  plt.ylabel('Frequency')
+  plt.title('Spectrogram of Mix (Sum)')
+  """
+  plt.tight_layout()
+  plt.show()
 
 def quick_asserts():
   # just to check some basic logic, not super exhaustive
@@ -159,6 +225,7 @@ def main():
   # change the numsamples parameter to change the number of triples of .wav files that are saved
   #produce_samples_v1(freqrange=[10000,15000],fileprefix="samplesv1_", numsamples=1, length=5, timerange=[0., 5.], countrange=[8,12], noiselevel=0.6, amplitude=2.5) # this was an arbitrary frequency range that might sound like bird chirps, feel free to tinker
   #write_single_advanced_chirp("advanced_chirp1.wav")
-  produce_samples_v2(freqrange=[5000,10000],fileprefix="samplesv2_", numsamples=1, length=5, timerange=[0., 5.], countrange=[8,12], noiselevel=0.6, amplitude=2.5)
+  tripletlist = produce_samples_v2(freqrange=[5000,10000],fileprefix="samplesv2_", numsamples=1, length=5, timerange=[0., 5.], countrange=[8,12], noiselevel=0.6, amplitude=2.5)
+  showspectrogram(makeSpectrogram(tripletlist[0][0])[2])
 if __name__ == "__main__":
   main()
